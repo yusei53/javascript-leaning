cf91c0cc0190fd4d9fc0649023da5d3c2fbca1f4
yusei53 <yusei530922@gmail.com>
2025-01-04 16:39:52 +0900

    add leaning03.js in learningC.string-processing

---
+++ learningC/string-processing/learning03.js
@@ -0,0 +1,17 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const [start, end] = lines[1].split(" ").map(Number);
+  const answer = lines[0].slice(start - 1, end);
+  console.log(answer);
+});
