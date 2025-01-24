be18c95983aca78ba0dfa6abab438f053495fd7f
yusei53 <yusei530922@gmail.com>
2025-01-24 17:48:18 +0900

    add leaning12.js in learningC.string-processing

---
+++ learningC/string-processing/learning12.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const s = lines[0].split("").reverse();
+  console.log(s.join(""));
+});
